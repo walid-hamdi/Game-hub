6d45ee0d4b47bc049f1bd579deddfc7ace11d3bd
Walid Hamdi <walidhamdidev@gmail.com>
2024-08-11 22:22:01 +0100

    Fetch the games

---
+++ src/services/api-client.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "100f85ddca2f4f598b0d091630757ae4",
+  },
+});
+
+export default apiClient;
