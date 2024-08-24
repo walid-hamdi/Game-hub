1b1be486c6763248964616b33171014e0957fd99
Walid Hamdi <walidhamdidev@gmail.com>
2024-08-24 18:56:06 +0100

    Ship platforms with app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -6,6 +6,7 @@ export interface Genre {
   image_background: string;
 }
 
+// const useGenres = () => useData<Genre>("/genres");
 const useGenres = () => ({ data: genres, error: null, isLoading: false });
 
 export default useGenres;
