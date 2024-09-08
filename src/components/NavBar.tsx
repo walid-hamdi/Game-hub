459a1f80658dc668bdde7bc548da82279a61ef18
Walid Hamdi <walidhamdidev@gmail.com>
2024-09-08 22:17:27 +0100

    Fix the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,10 +3,13 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => (
   <HStack padding="10px">
-    <Image src={logo} boxSize="60px" />
+    <Link to="/">
+      <Image src={logo} boxSize="60px" objectFit="cover" />
+    </Link>
     <SearchInput />
     <ColorModeSwitch />
   </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,16 +2,22 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BiSearch } from "react-icons/bi";
 import useGameQueryStore from "../store/store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const setSearch = useGameQueryStore((store) => store.setSearch);
   const ref = useRef<HTMLInputElement>(null);
+  const navigation = useNavigate();
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (ref.current) setSearch((ref.current as HTMLInputElement).value);
+
+        if (ref.current) {
+          setSearch((ref.current as HTMLInputElement).value);
+          navigation("/");
+        }
       }}
     >
       <InputGroup>
